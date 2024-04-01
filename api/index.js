cf5f944d638ef224f5fc010af93a01ddbaf1b614
jeiooo <angzcolao@gmail.com>
2024-04-01 14:47:46 +0800

    third com

--- api/index.js
+++ api/index.js
@@ -1,1 +1,15 @@
-//try
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+const url = 'mongodb://localhost:27017/capstone_prac';
+
+mongoose.connect(url, {})
+    .then(result => console.log('Connected to Database... '))
+    .catch(error => console.log(error.message));
+
+app.listen(3000, () => {
+    console.log('Connected to Port 3000... ');
+});
